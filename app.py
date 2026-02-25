f12931a85a9e61f8feb42d4d9bf1c0a5a77b2598
Steve <stevelevit230@gmail.com>
2026-02-25 12:02:22 +0200

    Add POST /api/chat/<room>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
@@ -19,6 +20,25 @@ def get_chat(room):
             return file.read()
     else:
         return ''
+    
+
+@app.route('/api/chat/<room>', methods=['POST'])
+def post_chat(room):
+    
+    username = request.form.get('username')
+    message = request.form.get('msg')
+
+ 
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    chat_line = f"[{current_time}] {username}: {message}\n"
+
+    filename = f"{room}.txt"
+    
+    with open(filename, 'a') as file:
+        file.write(chat_line)
+
+    return "Message saved successfully", 201
 
 if __name__ == '__main__': 
     app.run(debug=True)
