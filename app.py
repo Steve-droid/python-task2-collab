4953570db72e0406437034a621def45f35aa7cf7
orshokety <orshokety@gmail.com>
2026-02-25 11:43:00 +0200

    add get chat func

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app=Flask(__name__)
@@ -6,6 +7,15 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/api/chat/<room>', methods=['GET'])
+def get_chat(room):
+    filename = f"{room}.txt"
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            return file.read()
+    else:
+        return ''
 
 if __name__ == '__main__': 
     app.run(debug=True)
